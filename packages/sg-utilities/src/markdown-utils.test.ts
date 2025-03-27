075cd7f92c5098ecc71c1140493e648372805664
Giuseppe Caponetto <ggcaponetto@gmail.com>
2025-03-27 23:27:41 +0100

    add better gh workflows

--- packages/sg-utilities/src/markdown-utils.test.ts
+++ packages/sg-utilities/src/markdown-utils.test.ts
@@ -1,7 +1,7 @@
 import { describe, it, expect, vi, beforeEach, afterEach } from 'vitest';
 import * as fs from 'fs';
 import * as path from 'path';
-import { shiftHeadings, concatenateMarkdownFiles } from './markdown-util';
+import { shiftHeadings, concatenateMarkdownFiles } from './markdown-util.js';
 
 vi.mock('fs');
 
@@ -115,7 +115,7 @@ describe('runCli', () => {
   });
 
   it('reads input files and writes the output with shifted headings', async () => {
-    const runCli = (await import("./markdown-util")).runCli;
+    const runCli = (await import("./markdown-util.js")).runCli;
     runCli();
 
     expect(mockExit).not.toHaveBeenCalled();
