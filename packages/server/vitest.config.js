f62feca3cbb5067444710b746f165cfe6e86fb8d
Giuseppe Caponetto <ggcaponetto@gmail.com>
2025-03-29 10:04:08 +0100

    fix env file loading

---
+++ packages/server/vitest.config.js
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vitest/config';
+const thresholds = process.env.ENABLE_COVERAGE_TRESHOLD === 'true' ? {
+    autoUpdate: true,
+    statements: 86.56,
+    branches: 80,
+    functions: 87.5,
+    lines: 86.56,
+} : undefined;
+export default defineConfig({
+    test: {
+        coverage: {
+            enabled: true,
+            reportsDirectory: './coverage',
+            reportOnFailure: true,
+            provider: 'v8',
+            reporter: ['json-summary', 'html', 'text'],
+            include: [
+                "src/**/*"
+            ],
+            thresholds: thresholds
+        },
+    },
+});
