975353d77db107902c506f5b6bb3239d69458b92
Giuseppe Giulio Caponetto <ggcaponetto@gmail.com>
2025-03-31 07:31:09 +0200

    add radix ui

--- packages/desktop/src/components/main/main.tsx
+++ packages/desktop/src/components/main/main.tsx
@@ -1,9 +1,8 @@
 import { createRoot } from 'react-dom/client'
 import './main.css'
 import { version } from './../../../package.json'
-import "@radix-ui/themes/styles.css";
-import { Flex, Text, Button, Theme } from "@radix-ui/themes";
-
+import '@radix-ui/themes/styles.css'
+import { Flex, Text, Button, Theme } from '@radix-ui/themes'
 
 const { ipcRenderer } = window.electron
 
@@ -11,14 +10,21 @@ const root = createRoot(document.getElementById('root'))
 root.render(
     <Theme appearance="dark">
         <Flex direction="column" gap="2">
-        <Text style={{ textAlign: 'center' }} size={"5"}>Welcome to StreamGlass</Text>
-        <Text weight="light" size={"2"}>{version}</Text>
+            <Text style={{ textAlign: 'center' }} size={'5'}>
+                Welcome to StreamGlass
+            </Text>
+            <Text weight="light" size={'2'}>
+                {version}
+            </Text>
             <Button
                 onClick={async () => {
                     const result = await ipcRenderer.invoke?.('sg-event', {
                         message: 'This is a message from the renderer',
                     })
-                    console.log("Reveived a response from the event-handler: ", result)
+                    console.log(
+                        'Reveived a response from the event-handler: ',
+                        result
+                    )
                 }}
             >
                 invoke command
