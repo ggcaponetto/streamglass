ac527c771730c671cbefccdfbe0f4165ce3bb5c1
Giuseppe Caponetto <ggcaponetto@gmail.com>
2025-04-12 10:20:22 +0200

    wip docusaurus

---
+++ packages/docs/docs/intro.md
@@ -0,0 +1,8 @@
+---
+sidebar_position: 1
+---
+
+# StreamGlass Doumentation
+
+Let's discover the features of [https://streamglass.io](https://streamglass.io) **in less than 5 minutes**. Are you ready?
+Head over to the [Tutorial Basics](./tutorial-basics/quickstart.md) page.
---
+++ packages/docs/src/components/HomepageFeatures/index.tsx
@@ -0,0 +1,80 @@
+import type { ReactNode } from 'react';
+import clsx from 'clsx';
+import Heading from '@theme/Heading';
+import styles from './styles.module.css';
+
+import SvgMountain from '@site/static/img/undraw_docusaurus_mountain.svg';
+import SvgTree from '@site/static/img/undraw_docusaurus_tree.svg';
+
+interface FeatureItem {
+    title: string;
+    Svg: React.ComponentType<React.ComponentProps<'svg'>>;
+    description: ReactNode;
+}
+
+const FeatureList: FeatureItem[] = [
+    {
+        title: 'Easy to Use',
+        Svg: SvgMountain,
+        description: (
+            <>
+                StreamGlass was designed from the ground up to be easily
+                installed and used to get your website up and running quickly.
+            </>
+        ),
+    },
+    {
+        title: 'Focus on What Matters',
+        Svg: SvgTree,
+        description: (
+            <>
+                StreamGlass lets you focus on your workflow, and we&apos;ll do
+                the chores. Go ahead and define how you stream, your way.
+            </>
+        ),
+    },
+];
+
+function Feature({ title, Svg, description }: FeatureItem) {
+    return (
+        <div className={clsx('col col--4')}>
+            <div className="text--center">
+                <Svg className={styles.featureSvg} role="img" />
+            </div>
+            <div className="text--center padding-horiz--md">
+                <Heading as="h3">{title}</Heading>
+                <p>{description}</p>
+            </div>
+        </div>
+    );
+}
+
+export default function HomepageFeatures(): ReactNode {
+    return (
+        <section className={styles.features}>
+            <div
+                className="container"
+                style={{
+                    display: 'flex',
+                    flex: 1,
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                }}
+            >
+                <div
+                    className="row"
+                    style={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                    }}
+                >
+                    {FeatureList.map((props, idx) => (
+                        <Feature key={idx} {...props} />
+                    ))}
+                </div>
+            </div>
+        </section>
+    );
+}
