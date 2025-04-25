f5103764d28767500cc70267935f4157b6a1f78c
Giuseppe Giulio Caponetto <ggcaponetto@gmail.com>
2025-04-25 08:30:03 +0200

    fix navbar

--- packages/frontend/src/components/app/App.tsx
+++ packages/frontend/src/components/app/App.tsx
@@ -66,6 +66,7 @@ function App() {
                     breakpoint: 'sm',
                     collapsed: {
                         desktop: !opened,
+                        mobile: !opened,
                     },
                 }}
             >
