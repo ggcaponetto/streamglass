34684cd5a60561390bd7492e47abe3aed72b7f40
Giuseppe Caponetto <ggcaponetto@gmail.com>
2025-06-02 07:43:24 +0200

    wip

--- packages/frontend/src/components/mapping/Mapping.tsx
+++ packages/frontend/src/components/mapping/Mapping.tsx
@@ -1,15 +1,18 @@
 import { useRef, useEffect } from 'react';
 import * as monaco from 'monaco-editor/esm/vs/editor/editor.api';
-import defaultMapText from './maps/default?raw';
+import { State, useStore } from '../store/store';
+// import defaultMapText from './maps/default?raw';
 
 export const Mapping = () => {
     const monacoEl = useRef(null);
     const editorRef = useRef<monaco.editor.IStandaloneCodeEditor | null>(null);
+    const mapping = useStore((state: State) => state.mapping);
+    const setMapping = useStore((state: State) => state.setMapping);
     useEffect(() => {
         if (monacoEl.current) {
             const editorInstance = monaco.editor.create(monacoEl.current!, {
                 theme: 'vs-dark',
-                value: [defaultMapText].join('\n'),
+                value: mapping,
                 language: 'typescript',
             });
             editorInstance.onEndUpdate(() => {
@@ -18,11 +21,20 @@ export const Mapping = () => {
                     editorInstance,
                     content,
                 });
+                setMapping?.(content);
             });
             editorRef.current = editorInstance;
         }
         return () => editorRef.current?.dispose();
-    }, []);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [setMapping]);
+
+    useEffect(() => {
+        const current = editorRef.current;
+        if (current && mapping !== current.getValue()) {
+            current.setValue(mapping || '');
+        }
+    }, [mapping]);
 
     return (
         <div
--- packages/frontend/src/components/store/store.ts
+++ packages/frontend/src/components/store/store.ts
@@ -1,10 +1,14 @@
 import { create } from 'zustand';
 
+import defaultMapText from '../mapping/maps/default?raw';
+
 export type State = {
     isConnected: boolean;
     setIsConnected: (isConnected: boolean) => void;
     setSendCommand: (sendCommand: () => void) => void;
     sendCommand: (...args: unknown[]) => void;
+    mapping?: string;
+    setMapping?: (mapping: string) => void;
 };
 
 const useStore = create<State>((set) => ({
@@ -15,6 +19,8 @@ const useStore = create<State>((set) => ({
     sendCommand: () => {
         console.log('Not implemented yet');
     },
+    mapping: defaultMapText,
+    setMapping: (mapping: string) => set(() => ({ mapping })),
 }));
 
 export { useStore };
